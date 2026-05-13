74d44712faa656e2f0b34ae5c98614e4b561b7e9
mirdayan <dmirandaherrera@gmail.com>
2026-05-12 19:51:27 -0500

    creando una función reutilizable para obtener los datos de cada campo del formulario, añadiendo eventos de los campos a registrar.

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const pacienteInput = document.querySelector('#paciente');
 const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
-const sintomasInput = document.querySelectorAll('#sintomas');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#formulario-cita');
+
+
+// Eventos
+
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+formulario.addEventListener('submit', submitCita);
+
 
 
 // Objeto de Cita 
@@ -15,18 +29,21 @@ const citaObj = {
     email: '',
     fecha: '',
     sintomas: ''
-}
+};
 
-// Eventos
+// Funtion 
 
-pacienteInput.addEventListener('change', (event) => {
-   citaObj[event.target.name] = event.target.value
-
+function datosCita (event) {
+   citaObj[event.target.name] = event.target.value;
    console.log(citaObj);
-});
 
-propietarioInput.addEventListener('change', (event) => {
-   citaObj[event.target.name] = event.target.value
+};
 
-   console.log(citaObj);
-});
+
+function submitCita(event) {
+   event.preventDefault();
+
+   console.log('enviando datos del formulario....')
+
+};
+
