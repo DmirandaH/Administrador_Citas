9ad3338ad9b84ce3e6c9da933365a60795ae8f7c
mirdayan <dmirandaherrera@gmail.com>
2026-05-17 16:32:11 -0500

    almacenando las citas y evitar las alertas duplicadas

--- app.js
+++ app.js
@@ -31,6 +31,83 @@ const citaObj = {
     sintomas: ''
 };
 
+// Clase
+
+
+class Notificacion {
+
+   constructor ({texto, tipo}) {
+      this.texto = texto
+      this.tipo = tipo
+
+      this.mostrar()
+
+   }
+
+   //Método
+
+   mostrar() {
+      //Crear la notificación
+      const alerta = document.createElement('DIV');
+      alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+
+      //Eliminar alertas duplicadas
+
+      /*const alertaPrevia = document.querySelector('.alert') 
+      if(alertaPrevia) {
+         alertaPrevia.remove();
+      }*/
+
+      //Encadenamiento opcional para eliminar las alertas duplicadas
+
+      const alertaPrevia = document.querySelector('.alert') 
+      alertaPrevia?.remove();
+      
+
+
+
+
+      //Si es de tipo error, agrega una clase, usamos el operador ternario '?'
+      this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+
+      // Mensaje de error
+      alerta.textContent = this.texto
+
+      // Insertar en el DOM
+      formulario.parentElement.insertBefore(alerta, formulario);
+
+
+      // Quitar la alerta despúes de 3 segundos.
+
+      setTimeout(() => {
+         alerta.remove()
+         
+      }, 3000);
+
+
+   }
+
+}
+
+
+class AdminCitas {
+   constructor () {
+      this.citas = []     
+   }
+
+   // Método
+   agregar(cita) {
+      this.citas = [...this.citas, cita]
+
+
+      console.log(this.citas);
+   }
+}
+
+
+
 // Funtion 
 
 function datosCita (event) {
@@ -39,7 +116,7 @@ function datosCita (event) {
 
 };
 
-
+const citas = new AdminCitas()
 function submitCita(event) {
    event.preventDefault();
 
@@ -61,48 +138,15 @@ if(Object.values(citaObj).some(valor => valor.trim() === '')) {
       texto: 'Todos los campos son obligatorios',
       tipo: 'error'
    })
-
-  
-  
+ 
    return
 
    }
 
+   citas.agregar(citaObj)
+
 };
 
 
-// Clase
 
-
-class Notificacion {
-
-   constructor ({texto, tipo}) {
-      this.texto = texto
-      this.tipo = tipo
-
-      this.mostrar()
-
-   }
-
-   //Método
-
-   mostrar() {
-      //Crear la notificación
-      const alerta = document.createElement('DIV');
-      alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
-
-      //Si es de tipo error, agrega una clase, usamos el operador ternario '?'
-      this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
-
-
-      // Mensaje de error
-      alerta.textContent = this.texto
-
-      // Insertar en el DOM
-      formulario.parentElement.insertBefore(alerta, formulario);
-
-
-   }
-
-}
 
