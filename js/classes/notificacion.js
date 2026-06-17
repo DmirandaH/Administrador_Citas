0e1fb08e11034134b3d556b7e138f778ae783c91
mirdayan <dmirandaherrera@gmail.com>
2026-06-16 21:19:38 -0500

    Agregando módulos al proyecto, exportando e importando las nitificaciones.

---
+++ js/classes/notificacion.js
@@ -0,0 +1,56 @@
+export default class Notificacion {
+
+   constructor ({texto, tipo}) {
+      this.texto = texto
+      this.tipo = tipo
+
+      this.mostrar()
+
+   }
+
+   //Método
+
+   mostrar() {
+      //Crear la notificación
+      const alerta = document.createElement('DIV');
+      alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+
+      //Eliminar alertas duplicadas
+
+      /*const alertaPrevia = document.querySelector('.alert') 
+      if(alertaPrevia) {
+         alertaPrevia.remove();
+      }*/
+
+      //Encadenamiento opcional para eliminar las alertas duplicadas
+
+      const alertaPrevia = document.querySelector('.alert') 
+      alertaPrevia?.remove();
+      
+
+
+
+
+      //Si es de tipo error, agrega una clase, usamos el operador ternario '?'
+      this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+
+      // Mensaje de error
+      alerta.textContent = this.texto
+
+      // Insertar en el DOM
+      formulario.parentElement.insertBefore(alerta, formulario);
+
+
+      // Quitar la alerta despúes de 3 segundos.
+
+      setTimeout(() => {
+         alerta.remove()
+         
+      }, 3000);
+
+
+   }
+
+}
